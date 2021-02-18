648fc8abbf5c4c8201cb0b0c13c6f0834c28ac30
Chris TC Wong <christszchingwong@gmail.com>
2021-02-18 13:41:17 +0000

    setup prometheus middleware
    Note: the middleware does not work with version ^13.0.0

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+const prometheusMiddleware = require('express-prometheus-middleware');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -20,13 +21,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+// middlewares before routers
 app.use(function(req, res, next) {
   if (!req.headers[`mysecrettoken`]) {
     return next(createError(403));
   }
   next();
 });
+
+app.use(prometheusMiddleware());
 
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
