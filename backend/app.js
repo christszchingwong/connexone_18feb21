a57ef4eae2da95609b4d7fc45709109fff7407a2
Chris TC Wong <christszchingwong@gmail.com>
2021-02-18 21:18:41 +0000

    allow cors from frontend

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const prometheusMiddleware = require('express-prometheus-middleware');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
@@ -20,6 +21,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors({
+  origin: 'http://localhost:8080'
+}));
 
 // middlewares before routers
 app.use(function(req, res, next) {
